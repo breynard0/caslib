53ee452b2a4966fd5d69765b30e83c48f9358b69
breynard <dev@breynard.net>
2025-06-19 17:42:37 -0300

    Spoke too soon, had to fix roots.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "../include/trig.h"
 #include "../include/utils.h"
 #include "../include/dutils.h"
+#include "../include/root.h"
 #include <stdio.h>
 
 int main() {
     struct SinCos out = angle_pair(deg_to_rad(45.0));
     printf("%0.11f %0.11f\n", out.cos, out.sin);
+    printf("%0.11f\n", nth_root(2, 2.0));
     return 0;
 }
--- src/root.c
+++ src/root.c
@@ -6,7 +6,7 @@ double nth_root(unsigned int n, double d) {
 
     // Use double exponent to use square root of close-ish poewr of 2 as first estimate
     double_cast cast = (double_cast) d;
-    double estimate = 0x2 ^ ((cast.parts.exponent - 127) / 2);
+    double estimate = 0x2 ^ ((cast.parts.exponent - 1023) / 2);
 
     double fn = (double) n;
 
